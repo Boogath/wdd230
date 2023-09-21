93603f5a506e778da66f6462fc8b14d5b624b7de
Jacob Madsen <boogath@gmail.com>
2023-09-20 20:59:03 -0600

    CodePen added.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,11 @@ alert(document.lastModified);
 
 let date =  new Date().getFullYear();
 console.log(date);
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
