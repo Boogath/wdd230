c9ec71e904c35681eadda69e5b3b388e5ac32515
Jacob Madsen <boogath@gmail.com>
2023-09-22 21:49:37 -0600

    fixed dark/light.

--- scripts/dark.js
+++ scripts/dark.js
@@ -1,6 +1,8 @@
 const darkButton = document.querySelector('#dark');
 
-darkButton.addEventListener('click'), () => {
+darkButton.addEventListener('click', myFunction);
+
+function myFunction() {
     let content = darkButton.textContent;
     darkButton.textContent = content == 'Dark' ? 'Light' : 'Dark';
     document.body.classList.toggle('dark');
