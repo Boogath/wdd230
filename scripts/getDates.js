495aba77efea17cd33ba10b34c87ce5fbae84a12
Jacob Madsen <boogath@gmail.com>
2023-09-26 16:18:35 -0600

    fixing js.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,9 @@
-alert(document.lastModified);
+document.querySelector("#lastModified").textContent = "Las Modification: " + document.lastModified;
+
+const rightNow = new Date();
+console.log(rightNow);
+console.log(rightNow.getFullYear());
+document.querySelector("#year").textContent = rightNow.getFullYear();
 
 let date =  new Date().getFullYear();
 console.log(date);
