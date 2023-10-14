aa564c3eb4673b1d75f6ae8cc4f1707235dc447d
Jacob Madsen <boogath@gmail.com>
2023-10-14 17:54:01 -0600

    week 05.

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -5,9 +5,31 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
 
     const response = await fetch(url);
-    const data = await response.json;
-    console.table(data.prophets);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
 
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+    // cared building code goes here.
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portraite of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
